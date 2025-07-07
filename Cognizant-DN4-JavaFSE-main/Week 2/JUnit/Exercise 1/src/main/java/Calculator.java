87070a5c3ef935797bceacf1d24f640a0f2d3005
Ayush <turakayush@gmail.com>
2025-07-07 16:03:24 +0530

    Week 3

--- Cognizant-DN4-JavaFSE-main/Week 2/JUnit/Exercise 1/src/main/java/Calculator.java
+++ Cognizant-DN4-JavaFSE-main/Week 2/JUnit/Exercise 1/src/main/java/Calculator.java
@@ -2,4 +2,11 @@ public class Calculator {
     public int add(int a, int b) {
         return a + b;
     }
+
+    public static void main(String[] args) {
+        Calculator calc = new Calculator();
+        int result = calc.add(2, 3);
+        System.out.println("2 + 3 = " + result);
+    }
 }
+
